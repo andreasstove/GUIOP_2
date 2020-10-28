e7f10a0c605d86d93027bb1b2928679838a09695
andreasstove <andreasstove@outlook.dk>
2020-10-28 10:20:35 +0100

    update

--- GUIOP_1/GUIOP_1/View/AddCollecter.xaml.cs
+++ GUIOP_1/GUIOP_1/View/AddCollecter.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GUIOP_1
+namespace GUIOP_1.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddCollecter.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddCollecter : Window
     {
-        public MainWindow()
+        public AddCollecter()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
